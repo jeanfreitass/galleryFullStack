9cc2a63c9db453b0ab116a623d0d69d7f61c9c8f
Jean F <jean.freitas@iesb.edu.br>
2026-03-03 12:58:48 -0300

    upload projeto

---
+++ galleryapi/src/main/java/io/github/jeanfreitass/galleryapi/GalleryapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jeanfreitass.galleryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryapiApplication.class, args);
+	}
+
+}
---
+++ galleryapi/src/test/java/io/github/jeanfreitass/galleryapi/GalleryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeanfreitass.galleryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
